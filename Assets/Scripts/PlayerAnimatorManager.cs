3e0251144e81f3dfc3b12d4c6129c827c8becc9e
Seth <sjaugmented@gmail.com>
2021-06-10 17:38:32 -0700

    initial commit - ready to learn ownership

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,70 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sandbox
+{
+    public class PlayerAnimatorManager : MonoBehaviourPun
+    {
+        #region Private Fields
+
+        [SerializeField]
+        private float directionDampTime = 0.25f;
+        private Animator animator;
+
+        #endregion
+
+
+        #region MonoB Callbacks
+
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            if (!animator)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
+        }
+
+        private void Update()
+        {
+            if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+            {
+                return;
+            }
+            if (!animator)
+            {
+                return;
+            }
+            // deal with Jumping
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            // only allow jumping if we are running.
+            if (stateInfo.IsName("Base Layer.Run"))
+            {
+                // When using trigger parameter
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    animator.SetTrigger("Jump");
+                }
+            }
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (v < 0)
+            {
+                v = 0;
+            }
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+
+        }
+
+        #endregion
+
+
+
+    }
+}
+
+
